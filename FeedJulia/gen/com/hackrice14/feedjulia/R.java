5c8e839961cd1aa983c159a9114dc1e5f11a365c
Julia Hossu <iuli.hossu.2o@gmail.com>
2014-01-25 18:25:24 -0600

    made two layouts

--- FeedJulia/gen/com/hackrice14/feedjulia/R.java
+++ FeedJulia/gen/com/hackrice14/feedjulia/R.java
@@ -21,21 +21,43 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int creategroup=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int joingroup=0x7f020002;
+        public static final int passthephone=0x7f020003;
+        public static final int red=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f08000d;
+        public static final int button1=0x7f080008;
+        public static final int createGroup=0x7f08000b;
+        public static final int editText1=0x7f080002;
+        public static final int editText2=0x7f080006;
+        public static final int editText3=0x7f080004;
+        public static final int joinGroup=0x7f08000c;
+        public static final int mainLayout=0x7f080009;
+        public static final int passThePhone=0x7f08000a;
+        public static final int radioButton1=0x7f080001;
+        public static final int textView1=0x7f080003;
+        public static final int textView2=0x7f080000;
+        public static final int textView3=0x7f080007;
+        public static final int textView4=0x7f080005;
     }
     public static final class layout {
-        public static final int activity_start_screen=0x7f030000;
+        public static final int activity_group_settings=0x7f030000;
+        public static final int activity_start_screen=0x7f030001;
+        public static final int activity_survey=0x7f030002;
     }
     public static final class menu {
-        public static final int start_screen=0x7f070000;
+        public static final int group_settings=0x7f070000;
+        public static final int start_screen=0x7f070001;
+        public static final int survey=0x7f070002;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int title_activity_survey=0x7f050003;
     }
     public static final class style {
         /** 
