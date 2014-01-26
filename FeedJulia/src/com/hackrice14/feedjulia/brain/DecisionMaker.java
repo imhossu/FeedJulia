1552a3385129ac9fdec663d11bc9192a8fc31be3
Jeffrey Dudek <jeffreydudek@gmail.com>
2014-01-25 23:08:37 -0600

    Multiple keyword search with interleaving

--- FeedJulia/src/com/hackrice14/feedjulia/brain/DecisionMaker.java
+++ FeedJulia/src/com/hackrice14/feedjulia/brain/DecisionMaker.java
@@ -19,23 +19,47 @@ public class DecisionMaker {
 		
 		double top = -1000;
 		double next = -1000;
+		double third = -1000;
+		
 		String best = "";
 		String nextBest = "";
-		
+		String thirdBest = "";
 		for(String keyword : record.keySet()) {
 			double weight = record.get(keyword);
 			if(weight > top) {
+				thirdBest = nextBest;
 				nextBest = best;
 				best = keyword;
+				
+				third = next;
 				next = top;
 				top = weight;
 			} else if(weight > next) {
-				next = weight;
+				thirdBest = nextBest;
 				nextBest = keyword;
+				
+				third = next;
+				next = weight;
+			} else if(weight > third) {
+				thirdBest = keyword;
+				third = weight;
 			}
 		}
 		
 		PlacesAPI lookup = new PlacesAPI();
-		return lookup.getPlaces(lat, lon, new String[]{best});
+		ArrayList<RestaurantInfo> topMatches = lookup.getPlaces(lat, lon, new String[]{best});
+		ArrayList<RestaurantInfo> nextMatches = lookup.getPlaces(lat, lon, new String[]{nextBest});
+		ArrayList<RestaurantInfo> thirdMatches = lookup.getPlaces(lat, lon, new String[]{thirdBest});
+		
+		ArrayList<RestaurantInfo> result = new ArrayList<RestaurantInfo>();
+		for(int i = 0; i < topMatches.size() || i < nextMatches.size() || i < thirdMatches.size(); i++) {
+			if(i < topMatches.size())
+				result.add(topMatches.get(i));
+			if(i < nextMatches.size())
+				result.add(nextMatches.get(i));
+			if(i < thirdMatches.size())
+				result.add(thirdMatches.get(i));
+		}
+		return result;
 	}
 }
